dde8c63cf6d972093310bbd9820296898dcbf51d
charissazahra <charissazahra11@gmail.com>
2025-06-16 20:16:58 +0700

    Add logic to save movie object to movie_data.json

--- src/api/movie.py
+++ src/api/movie.py
@@ -1,16 +1,35 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter  
 from src.models.movie import Movie
+import json
+from pathlib import Path 
 
-router = APIRouter()
+router = APIRouter() 
 
 @router.post("/movies/", response_model=Movie)
 def add_movie(movie: Movie):
-	print(f"movie title: {movie.title}") #nge-cetak atribut dari object movie
-	print(f"movie id: {movie.id}")
-	print(f"movie description: {movie.description}")
-	print(f"movie duration: {movie.duration}")
-	print(f"movie rating: {movie.rating}")
-	print(f"movie cover: {movie.cover}")
+	movie_dict = movie.dict()
+
+	print(movie_dict)
+	file_path = Path(__file__).resolve().parent.parent / "data" / "movie_data.json"
+
+
+	if not file_path.exists():
+		raise FileNotFoundError("File movie_data tidak ditemukan. Pastikan sudah dibuat secara manual dulu")
+
+	with open(file_path, "r", encoding="utf-8") as f:
+		try:
+			data = json.load(f)
+		except json.JSONDecodeError:
+			data = []
+
+	data.append(movie_dict)
+
+	with open(file_path, "w", encoding="utf-8") as f:
+		try:
+			json.dump(data, f, indent=4)
+		except Exception as e:
+			print(e)
+
+
 	return movie
-	
+
